f807adf1b11b93ddc484221f0c46c71c27dbecd3
Vlad Sushchenko <71527547+6Glow@users.noreply.github.com>
2024-07-05 19:58:12 +0300

    feat: Add animation for swaps

--- script.js
+++ script.js
@@ -1,40 +1,59 @@
-const n = 10;
-const array = [];
+const n=46;
+const array=[];
+
 
 init();
 
 function init(){
-  for(let i = 0; i < n; i++) {
-    array[i] = Math.random();
-  }
-  showBars()
-}
-
-function play(){
-    bubbleSort(array);
+    for(let i=0;i<n;i++){
+        array[i]=Math.random();
+    }
     showBars();
 }
 
-function bubbleSort(array){
-  do{
-    var swapped = false;
-    for(let i=1; i < array.length; i++) {
-      if(array[i-1]> array[i]){
-        swapped=true;
-        [array[i-1], array[i]] = [array[i],array[i-1]];
-      }
+function play(){
+    const swaps=bubbleSort([...array]);
+    animate(swaps);
+}
+
+function animate(swaps){
+    if(swaps.length==0){
+        showBars();
+        return;
     }
-  }while(swapped);
+    const [i,j]=swaps.shift(0);
+    [array[i],array[j]]=[array[j],array[i]];
+    showBars([i,j]);
+    setTimeout(function(){
+        animate(swaps);
+    },200);
 }
 
-
-function showBars(){
-  container.innerHTML="";
-  for(let i=0; i<array.length; i++) {
-    const bar = document.createElement('div');
-    bar.style.height = array[i]*100+"%";
-    bar.style.width = "10px";
-    bar.classList.add("bar");
-    container.appendChild(bar);
-  }
+function bubbleSort(array){
+    const swaps=[];
+    do{
+        var swapped=false;
+        for(let i=1;i<array.length;i++){
+            if(array[i-1]>array[i]){
+                swaps.push([i-1,i]);
+                swapped=true;
+                [array[i-1],array[i]]=[array[i],array[i-1]];
+            }
+        }
+    }while(swapped);
+    return swaps;
 }
+
+function showBars(indices){
+    container.innerHTML="";
+    for(let i=0;i<array.length;i++){
+        const bar=document.createElement("div");
+        bar.style.height=array[i]*100+"%";
+        bar.classList.add("bar");
+        if(indices && indices.includes(i)){
+            bar.style.backgroundColor="PURPLE";
+        }
+        container.appendChild(bar);
+    }   
+}
+
